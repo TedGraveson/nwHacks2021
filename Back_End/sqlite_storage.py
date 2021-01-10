c44d59f7a5cad9152ba69dc5bbb9ad679247e5f4
Ted Graveson <ted.graveson@gmail.com>
2021-01-10 15:12:43 -0400

    Update sqlite_storage.py

--- Back_End/sqlite_storage.py
+++ Back_End/sqlite_storage.py
@@ -147,19 +147,15 @@ def distance_compare(address, fName, lName, res_user, res_order):
     valid = []
     for i in range(len(res_order)):
         if((fName != res_order[i][0]) and (lName != res_order[i][1])):
+            timeE = res_order[i][5]
+            tip = res_order[i][7]
             dist = distance(address, res_order[i][6])
             fir = res_order[i][0]
             las = res_order[i][1]
-            res = [dist, fir, las]
+            res = [dist, fir, las, timeE, tip]
             valid.append(res)
 
     valid.sort(key=take_first)
-    #at this point there should be a list of of people that are of the opposite is_Driver bool
-    #and do not have the exact same name as the person we are checking (this could imply) a repeat
-    #order if the aforementioned statement were true. The final thing we do is sort the list based
-    #on the distance, so now the first index will be the lowest distance away (by extension we can
-    # later call the person since the list also stores the person's first and last name).
-    # this means that index 0 in valid is the closest possible person to the address passed in.
 
     return valid
 
